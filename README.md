bf7e2fcd4633057a00b7da574a448f8da1b10635
Jherimum <eugenio.perrottaneto@gmail.com>
2023-06-12 18:24:07 -0300

    adding references

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ https://www.zupzup.org/rust-job-runner/
 https://github.com/JoeyMckenzie/realworld-rust-axum-sqlx/tree/main
 https://fasterthanli.me/articles/remote-development-with-rust-on-fly-io#what-the-heck-is-fly-io-for-even
 https://dpc.pw/data-oriented-cleanandhexagonal-architecture-software-in-rust-through-an-example
+https://users.rust-lang.org/t/reborrowing-via-a-trait/80039/5
--- crates/perroute-storage/src/models/mod.rs
+++ crates/perroute-storage/src/models/mod.rs
@@ -1,6 +1,54 @@
+use std::str::FromStr;
+
+use perroute_commons::types::code::Code;
+use sqlx::{Executor, PgExecutor, PgPool};
+
+use self::channel::Channel;
+
 pub mod api_key;
 pub mod channel;
 pub mod command_log;
 pub mod connection;
 pub mod user;
 pub mod user_password;
+
+pub struct Repository<E> {
+    executor: E,
+}
+
+pub trait WithExecutor<'e> {
+    type Executor: Executor<'e, Database = sqlx::postgres::Postgres>;
+
+    fn executor(&'e mut self) -> Self::Executor;
+}
+
+impl<'p> WithExecutor<'p> for Repository<sqlx::postgres::PgPool> {
+    type Executor = &'p sqlx::postgres::PgPool;
+
+    fn executor(&'p mut self) -> Self::Executor {
+        &self.executor
+    }
+}
+
+impl<'t, 'c> WithExecutor<'t> for Repository<sqlx::Transaction<'c, sqlx::Postgres>>
+where
+    'c: 't,
+{
+    type Executor = &'t mut sqlx::Transaction<'c, sqlx::Postgres>;
+
+    fn executor(&'t mut self) -> Self::Executor {
+        &mut self.executor
+    }
+}
+
+impl<'e, T> Repository<T>
+where
+    Self: WithExecutor<'e>,
+{
+    pub async fn submit(&'e mut self) {
+        Channel::new(&Code::from_str("CODE").unwrap(), "ss")
+            .save(self.executor())
+            .await
+            .unwrap();
+    }
+}
