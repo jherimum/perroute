ce5622a5b358cff65a5cecd5f5cd8b2b39ec21a4
Jherimum <eugenio.perrottaneto@gmail.com>
2023-06-24 11:58:44 -0300

    adding routes

--- crates/perroute-backoffice-api/src/rest/routes/message_type_version_router.rs
+++ crates/perroute-backoffice-api/src/rest/routes/message_type_version_router.rs
@@ -1,11 +1,53 @@
-use axum::Router;
-
 use crate::rest::Buses;
+use axum::{
+    response::IntoResponse,
+    routing::{delete, get, post, put},
+    Router,
+};
 
 pub struct MessageTypeVersionRouter;
 
 impl MessageTypeVersionRouter {
     pub fn routes(buses: Buses) -> Router {
         Router::new()
+            .nest(
+                "/v1/channels/:channel_id/message_types/:message_type_id/versions",
+                Router::new()
+                    .route("/", get(Self::query_versions))
+                    .route("/", post(Self::create_version))
+                    .nest(
+                        "/:id",
+                        Router::new()
+                            .route("/", get(Self::find_version))
+                            .route("/", put(Self::update_version))
+                            .route("/", delete(Self::delete_version))
+                            .route("/publish", post(Self::publish_version)),
+                    ),
+            )
+            .with_state(buses)
+    }
+
+    async fn query_versions() -> impl IntoResponse {
+        todo!()
+    }
+
+    async fn create_version() -> impl IntoResponse {
+        todo!()
+    }
+
+    async fn update_version() -> impl IntoResponse {
+        todo!()
+    }
+
+    async fn delete_version() -> impl IntoResponse {
+        todo!()
+    }
+
+    async fn find_version() -> impl IntoResponse {
+        todo!()
+    }
+
+    async fn publish_version() -> impl IntoResponse {
+        todo!()
     }
 }
