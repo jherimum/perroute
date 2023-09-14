a2df7663e5e8eb3352e4977752954e7766e36eb5
Jherimum <eugenio.perrottaneto@gmail.com>
2023-09-14 18:41:44 -0300

    initi integration

--- crates/perroute-integration/src/main.rs
+++ crates/perroute-integration/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use perroute_commons::{configuration::settings::Settings, tracing::init_tracing};
+use perroute_messaging::rabbitmq::connection::RabbitmqConnection;
+use perroute_storage::{connection_manager::ConnectionManager, error::StorageError};
+use sqlx::PgPool;
+use tap::TapFallible;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    dotenv::dotenv().ok();
+    init_tracing();
+    let settings =
+        Settings::load().tap_err(|e| tracing::error!("Error loading settings. Error: {e}"))?;
+
+    let publisher = conn(&settings).await?;
+    let pool = build_pool(&settings).await?;
+
+    Ok(())
+}
+
+async fn conn(settings: &Settings) -> Result<RabbitmqConnection, anyhow::Error> {
+    Ok(RabbitmqConnection::connect(settings.into()).await?)
+}
+
+async fn build_pool(settings: &Settings) -> Result<PgPool, StorageError> {
+    ConnectionManager::build_pool(&settings.database)
+        .await
+        .tap_err(|e| tracing::error!("Failed to build connection poll:{e}"))
 }
